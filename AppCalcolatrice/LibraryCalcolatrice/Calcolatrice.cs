b6a9529361cbbaa3efbb1f0b1ed9a76792754e02
LabTepsit <labinfo4@secondarieclago.it>
2018-10-27 14:31:21 +0200

    1

---
+++ AppCalcolatrice/LibraryCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibraryCalcolatrice
+{
+    public class Calcolatrice
+    {
+        public double Risultato { get; set; }
+        public double Divisione(double v)
+        {
+            if(v==0)
+           { throw new System.Exception("valoe non valido"); }
+            Risultato /= v;
+            return Risultato;
+        }
+    }
+}
---
+++ AppCalcolatrice/LibraryCalcolatriceTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryCalcolatrice;
+
+namespace LibraryCalcolatriceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDivisioneZero()
+        {
+            Calcolatrice c = new Calcolatrice();
+            c.Risultato = 1;
+            try
+            {
+                c.Divisione(0);
+                Assert.Fail(); // If it gets to this line, no exception was thrown
+            }
+            catch (Exception) { }
+        }
+    }
+}
